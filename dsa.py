3191b6fc6f7ffa67a016a6654e756fa5f4d9d49f
John Tenkorang Anim <johntenkoranganim@gmail.com>
2025-06-27 15:38:34 -0400

    Included Definition of binary tree node

--- dsa.py
+++ dsa.py
@@ -83,12 +83,12 @@ class Solution:
 
         return spiral_matrix
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
         pre_idx = 0
