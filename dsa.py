808d028a19bdf5cf32b72cf748c6f5dc972b11c4
John Tenkorang Anim <johntenkoranganim@gmail.com>
2025-06-30 15:24:17 -0400

    findCenter

--- dsa.py
+++ dsa.py
@@ -211,3 +211,12 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return res
+    
+    def findCenter(self, edges: List[List[int]]) -> int:
+        a, b = edges[0]
+        c, d = edges[1]
+        
+        if a == c or a == d:
+            return a
+        else:
+            return b
