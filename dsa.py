63470cef98394a972091109c7ce9ca7d6b8c8c9f
John Tenkorang Anim <johntenkoranganim@gmail.com>
2025-06-30 12:36:04 -0400

    isSubTree

--- dsa.py
+++ dsa.py
@@ -124,3 +124,25 @@ class Solution:
             return False
 
         return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return isSameTree(p.right,q.right) and isSameTree(p.left,q.left)
+        
+        def dfs(root, subRoot):
+            if not root:
+                return False
+
+            if isSameTree(root,subRoot):
+                return  True
+            return dfs(root.left,subRoot) or dfs(root.right, subRoot)
+
+        return dfs(root, subRoot)
