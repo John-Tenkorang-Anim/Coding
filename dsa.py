da5ba43de2cafdb5e5932d6bcf5db3b7d55b7dbf
John Tenkorang Anim <johntenkoranganim@gmail.com>
2025-07-11 14:19:28 -0400

    Twitter

--- dsa.py
+++ dsa.py
@@ -395,4 +395,34 @@ def leastInterval(self, tasks: List[str], n: int) -> int:
 
         return time
 
-       
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.tweets = defaultdict(list)
+        self.following = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.time += 1
+        self.tweets[userId].append((self.time, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        heap = []
+
+        for tweet in self.tweets[userId][-10:]:
+            hq.heappush(heap, tweet)
+        
+        for followee in self.following[userId]:
+            for t in self.tweets[followee][-10:]:
+                hq.heappush(heap, t)
+        
+        return [tweetId for _,tweetId in hq.nlargest(10, heap)]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.following[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
+      
