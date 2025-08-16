cbdd187a762bb3d764508109ed2ad0ccd8850ea4
John Tenkorang Anim <johntenkoranganim@gmail.com>
2025-08-16 17:59:05 -0400

    surrounded regions

--- dsa.py
+++ dsa.py
@@ -663,3 +663,44 @@ class Solution:
                 if (r,c) in aset and (r, c) in pset:
                     res.append([r,c])
         return res
+
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r,c):
+
+            if not ( 0 <= r < rows and 0 <= c < cols) or board[r][c] != "O":
+                return
+
+            board[r][c] = "T"
+
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
+        
+        #starting on top and bottom
+        for c in range(cols):
+            if board[0][c] == "O":
+                dfs(0,c)
+            if board[rows-1][c] == "O":
+                dfs(rows-1,c)
+        
+        # checking the vertical borders
+        for r in range(rows):
+            if board[r][0] == "O":
+                dfs(r,0)
+            if board[r][cols-1] == "O":
+                dfs(r,cols-1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
+        
